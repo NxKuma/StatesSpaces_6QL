bc38fb170536f3294f7e9023481ab9b8e85ed224
Jade <jade.ramos@student.ateneo.edu>
2024-11-30 00:39:28 +0800

    Updated User Management

--- stateSpaces/user_management/models.py
+++ stateSpaces/user_management/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    customer = models.OneToOneField('reservations.Customer', on_delete=models.CASCADE)
     display_name = models.CharField(max_length = 63)
     email_address = models.EmailField(max_length = 254)
 
