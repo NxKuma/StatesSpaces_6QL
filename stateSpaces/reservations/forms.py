d298bde7457b0b351459ca6798ff0458f0934701
NxKuma <neilisraelko01@gmail.com>
2024-12-02 06:47:53 +0800

    Trying to Implement CSS

--- stateSpaces/reservations/forms.py
+++ stateSpaces/reservations/forms.py
@@ -8,6 +8,26 @@ class ReserveForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = '__all__'
+        widgets = {
+            'date_start' : forms.widgets.DateInput(attrs={'type': 'date'}),
+            'date_end' : forms.widgets.DateInput(attrs={'type': 'date'}),
+        }
+
+    def is_valid(self):
+        valid = super().is_valid()
+
+        # Double Check
+        # 1: if no data was posted, cleaned_data won't exist - empty form submit
+        # 2: if valid: all required fields (start + end) are valid // is not None
+        if hasattr(self, 'cleaned_data') and valid:
+
+            start_date = self.cleaned_data.get('date_start')
+            end_date = self.cleaned_data.get('date_end')
+            if end_date < start_date:
+                self.add_error('date_end', 'End date must be greater than start date')
+                valid = False
+
+        return valid
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- stateSpaces/user_management/forms.py
+++ stateSpaces/user_management/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.conf import settings
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
     display_name = forms.CharField(max_length=255)
     email = forms.EmailField()
-    birth_date = forms.DateField(widget=forms.SelectDateWidget)
+    birth_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
